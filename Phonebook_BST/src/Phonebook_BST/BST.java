eaf64453b1b92212a93a81e37527be79d857d0e4
Raghad <145941111+Raghadr9@users.noreply.github.com>
2023-11-26 17:03:11 +0300

    Update BST.java

--- Phonebook_BST/src/Phonebook_BST/BST.java
+++ Phonebook_BST/src/Phonebook_BST/BST.java
@@ -51,10 +51,6 @@ public class BST {
 		}
 	}
 
-	//public boolean insertContact(String key, Contact val) {
-	//	return insertContact(root, key, val);
-	//}
-
 	public boolean insertContact(String key, Contact val) {
 		BSTNode p, q = current;
 
@@ -75,20 +71,13 @@ public class BST {
 			return true;
 		}
 
-		/*
-		 * if (root == null) { BSTNode newNode = new BSTNode(key, val); current =
-		 * newNode; return newNode; } if (key.compareTo(root.key) == 0) return null;
-		 * 
-		 * if (key.compareTo(root.key) < 0) root.left = insertContact(root.left, key,
-		 * val); else root.right = insertContact(root.right, key, val);
-		 * 
-		 * return null;
-		 */
 	}
-public boolean findkey(String key) {
-	return findkey(root, key);
-}
-	private boolean findkey(BSTNode root,String key) {
+
+	public boolean findkey(String key) {
+		return findkey(root, key);
+	}
+
+	private boolean findkey(BSTNode root, String key) {
 		BSTNode p = root, q = root;
 		if (empty())
 			return false;
@@ -116,38 +105,24 @@ public boolean findkey(String key) {
 			return true;
 
 		boolean right = isExist(root.right, phoneNum);
-		
+
 		return right || left;
 	}
 
 	public boolean isExist(Contact contact) {
-		return isExist(root,contact.getPhoneNumber());
+		return isExist(root, contact.getPhoneNumber());
 	}
 
 	private boolean Search(BSTNode p, String n, String data) {
-		boolean found = false , left,right ;
+		boolean found = false, left, right;
 		switch (n) {
-		case "PhoneNumber":
-			
-			if (p == null)
-				return false;
-			
-			left=Search(p.left, n, data);
-			
-			if (p.data.getPhoneNumber().equals(data)) {
-				System.out.println(p.data.toString());
-				return true;
-			}
-			right= Search(p.right, n, data);
-			found = left || right;
-			break;
 
 		case "FirstName":
 			if (p == null)
 				return false;
-			
-			left=Search(p.left, n, data);
-			
+
+			left = Search(p.left, n, data);
+
 			int index = p.data.getName().indexOf(" ");
 			if (index != -1) {
 				String firstName = p.data.getName().substring(0, index);
@@ -155,23 +130,22 @@ public boolean findkey(String key) {
 					System.out.println(p.data.toString());
 				return true;
 			}
-			
-			right=Search(p.right, n, data);
-			found = left||right;
-			break;
 
+			right = Search(p.right, n, data);
+			found = left || right;
+			break;
 
 		case "Email":
 			if (p == null)
 				return false;
-			
-			left=Search(p.left, n, data);
+
+			left = Search(p.left, n, data);
 
 			if (p.data.getEmail().equals(data)) {
 				System.out.println(p.data.toString());
 				return true;
 			}
-			right= Search(p.right, n, data);
+			right = Search(p.right, n, data);
 			found = left || right;
 			break;
 
@@ -179,27 +153,27 @@ public boolean findkey(String key) {
 			if (p == null)
 				return false;
 
-			left=Search(p.left, n, data);
+			left = Search(p.left, n, data);
 
 			if (p.data.getBirthday().equals(data)) {
 				System.out.println(p.data.toString());
 				return true;
 			}
-			right= Search(p.right, n, data);
+			right = Search(p.right, n, data);
 			found = left || right;
 			break;
 
 		case "Address":
 			if (p == null)
 				return false;
-			
-			left=Search(p.left, n, data);
-			
+
+			left = Search(p.left, n, data);
+
 			if (p.data.getAddress().equals(data)) {
 				System.out.println(p.data.toString());
 				return true;
 			}
-			right= Search(p.right, n, data);
+			right = Search(p.right, n, data);
 			found = left || right;
 			break;
 		}
@@ -219,77 +193,90 @@ public boolean findkey(String key) {
 			return null;
 
 		if (p.data.getName().equalsIgnoreCase(name)) {
-			return p.data ;
-		}
-		if (name.compareTo(root.data.getName()) < 0)
-		return SearchByName(p.left, name);
+			return p.data;
+		} else if (name.compareTo(root.data.getName()) < 0)
+			return SearchByName(p.left, name);
 		else
-		return SearchByName(p.right, name);
+			return SearchByName(p.right, name);
 	}
-	
-	
-	
-	public boolean removekey(String key) {
-		
-	String k1 = key;
-	BSTNode p = root;
-	BSTNode q = null;	
-	
-	while(p != null) {
-	if (k1.compareToIgnoreCase(p.key) < 0) {
-		q = p;
-		p = p.left;
-	} else if (k1.compareToIgnoreCase(p.key) > 0) {
-		q = p;
-		p = p.right;
-	}else {
-		
-		if((p.left != null)&& (p.right != null)) {
-			
-			BSTNode min = p.right;
-			q = p;
-			while (min.left != null) {
-				
-				q = min;
-				min = min.left;
-			}
-			    p.key = min.key;
-				p.data = min.data;
-				k1 = min.key;
-				p = min;
-			}
-			
-			if (p.left != null) {
-				p = p.left;
-				
-			} else { p = p.right; }
-			
-			if (q == null ) {
-				root = p;
-				
-			} else { 
-				if(k1.compareToIgnoreCase(q.key) < 0) {
-					q.left = p;
-				} else { q.right = p; }
-				
-			}
-			current = root;
-			return true;
-			
+
+	public Contact SearchByPhoneNumber(String PhoneNumber) {
+		return SearchByPhoneNumber(root, PhoneNumber);
+	}
+
+	private Contact SearchByPhoneNumber(BSTNode p, String phoneNumber) {
+		if (p == null) {
+			return null;
 		}
-		
+
+		if (p.data.getPhoneNumber().equalsIgnoreCase(phoneNumber)) {
+			return p.data;
+		}
+
+		Contact leftResult = SearchByPhoneNumber(p.left, phoneNumber);
+		if (leftResult != null) {
+			return leftResult;
+		}
+
+		return SearchByPhoneNumber(p.right, phoneNumber);
 	}
-	return false;
-	
+
+	public boolean removekey(String key) {
+
+		String k1 = key;
+		BSTNode p = root;
+		BSTNode q = null;
+
+		while (p != null) {
+			if (k1.compareToIgnoreCase(p.key) < 0) {
+				q = p;
+				p = p.left;
+			} else if (k1.compareToIgnoreCase(p.key) > 0) {
+				q = p;
+				p = p.right;
+			} else {
+
+				if ((p.left != null) && (p.right != null)) {
+
+					BSTNode min = p.right;
+					q = p;
+					while (min.left != null) {
+
+						q = min;
+						min = min.left;
+					}
+					p.key = min.key;
+					p.data = min.data;
+					k1 = min.key;
+					p = min;
+				}
+
+				if (p.left != null) {
+					p = p.left;
+
+				} else {
+					p = p.right;
+				}
+
+				if (q == null) {
+					root = p;
+
+				} else {
+					if (k1.compareToIgnoreCase(q.key) < 0) {
+						q.left = p;
+					} else {
+						q.right = p;
+					}
+
+				}
+				current = root;
+				return true;
+
+			}
+
+		}
+		return false;
+
 	}
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
